ca902d0bc113c8ed0f01d145da780fd346f55449
La Negra <La Negra@Lanegra-Ros>
2021-02-15 19:26:15 -0500

    Added the if statement to obtain the add numbers

--- src/EvenNumbersHomework.java
+++ src/EvenNumbersHomework.java
@@ -23,6 +23,12 @@ public class EvenNumbersHomework {
         // Creates a loop to create the count of the numbers from 0 to 100
         for(numbs= 0; numbs<=100; numbs++){
             
+         /*Creates the if statement that will search for the even numbers
+         and excludes the number 0 from the result printed */
+            if(numbs % 2 == 0 && numbs != 0 )
+                
+            { 
+
         }
          
 
